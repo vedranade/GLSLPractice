0d2fecc8b8d7951bfc30b66ecaa8d6fad85f9cf0
Ved Ranade <ved.ranade@gmail.com>
2019-05-31 20:28:54 -0400

    Added color as another vertex attribute.

--- GLSLTest/main.cpp
+++ GLSLTest/main.cpp
@@ -9,15 +9,20 @@
 
 const char *vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aCol;\n"
+"out vec3 Color;\n"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
+"   gl_Position = vec4(aPos, 1.0);\n"
+"   Color = aCol;\n"
+"}\n\0";
+
 const char *fragmentShaderSource = "#version 330 core\n"
+"in vec3 Color;\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"   FragColor = vec4(Color, 1.0f);\n"
 "}\n\0";
 
 int main()
@@ -94,11 +99,11 @@ int main()
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	GLfloat vertices[] = {
-		0.5f, 0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		-0.5f, -0.5f, 0.0f,
-		-0.5f, 0.5f, 0.0f
+	GLfloat vertices[] = 
+	{
+		0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+		0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+		0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f
 	};
 
 	GLuint indices[] =
@@ -108,8 +113,6 @@ int main()
 	};
 
 	GLuint VAO, VBO, EBO;
-
-	
 
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -122,8 +125,11 @@ int main()
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	do
 	{
@@ -131,9 +137,9 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shaderProgram);
+
 		glBindVertexArray(VAO);
-		//glDrawArrays(GL_TRIANGLES, 0, 3);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		//// Swap buffers
 		glfwSwapBuffers(window);
